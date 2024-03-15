3e931b76528ae08e06da6a8c0b8bab6bb0083bd8
JongMinCh0i <wonderland314592@gmail.com>
2024-03-15 14:55:09 +0900

    예제 프로젝트 만들기 v2

--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -1,17 +1,11 @@
 package hello.proxy.app.v1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /*
-   @Controller Or @RequestMapping 이 있어야 스프링 컨트롤러로 인식
-   꼭 @Controller 나 @RestController 가 있어야만 컨트롤러가 되는 것이 아님
+   3.0 기준 : @Controller Or @RequestMapping 이 있어야 스프링 컨트롤러 인식
 */
-//
-@RequestMapping
-@ResponseBody
+@RestController
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")
